e031f07df61d879111d50c885ebaeb4404b442a5
ErenKaptan4 <erenkaptanalp@gmail.com>
2024-05-06 17:04:17 +0200

    Terrain Texture
    alphamap done and terrain has texture

--- Assets/Scripts/GenerateRandomHeight.cs
+++ Assets/Scripts/GenerateRandomHeight.cs
@@ -2,6 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable] public class TerrainTextureData
+{
+    public Texture2D terrainTexture;
+
+    public Vector2 tileSize;
+
+    public float minHeight;
+    public float maxHeight;
+
+}
+
 public class GenerateRandomHeight : MonoBehaviour
 {
     private Terrain terrain;
@@ -13,9 +24,14 @@ public class GenerateRandomHeight : MonoBehaviour
     [SerializeField] [Range(0f, 1f)] private float maxRandomHeightRange = 0.1f;
 
     [SerializeField] bool flattenTerrain = true;
+
     [Header("Perlin Noise")][SerializeField] private bool perlinNoise = false;
     [SerializeField] private float perlinNoiseWidthScale = 0.01f;
     [SerializeField] private float perlinNoiseHeightScale = 0.01f;
+
+    [Header("Texture Data")][SerializeField] private List<TerrainTextureData> terrainTextureData;
+    [SerializeField] private bool addTerrainTexture = false;
+    [SerializeField] private float terrainTextureBlendOffset = 0.01f;
 
     // Start is called before the first frame update
     void Start()
@@ -32,6 +48,7 @@ public class GenerateRandomHeight : MonoBehaviour
         }
 
         GenerateHeight();
+        addTerrainTextures();
 
     }
 
@@ -48,7 +65,7 @@ public class GenerateRandomHeight : MonoBehaviour
         {
             for(int height = 0; height < terrainData.heightmapResolution; height++)
             {
-                /*
+                
                 if (perlinNoise)
                 {
                     heightMap[width, height] = Mathf.PerlinNoise(width * perlinNoiseWidthScale, height * perlinNoiseHeightScale);
@@ -58,9 +75,7 @@ public class GenerateRandomHeight : MonoBehaviour
                 {
                     heightMap[width, height] = Random.Range(minRandomHeightRange, maxRandomHeightRange); 
                 }
-                */
-                heightMap[width, height] += Mathf.PerlinNoise(width * perlinNoiseWidthScale, height * perlinNoiseHeightScale);
-                heightMap[width, height] += Random.Range(minRandomHeightRange, maxRandomHeightRange);
+                
             }
         }
 
@@ -81,6 +96,84 @@ public class GenerateRandomHeight : MonoBehaviour
 
         terrainData.SetHeights(0, 0, heightMap);
     }
+
+    private void addTerrainTextures()
+    {
+        TerrainLayer[] terrainLayers = new TerrainLayer[terrainTextureData.Count];
+
+        for (int i = 0; i < terrainTextureData.Count; i++)
+        {
+            if (addTerrainTexture)
+            {
+                terrainLayers[i] = new TerrainLayer();
+                terrainLayers[i].diffuseTexture = terrainTextureData[i].terrainTexture;
+                terrainLayers[i].tileSize = terrainTextureData[i].tileSize;
+            }
+            else
+            {
+                terrainLayers[i] = new TerrainLayer();
+                terrainLayers[i].diffuseTexture = null;
+            }
+
+        }
+
+        terrainData.terrainLayers = terrainLayers;
+
+        float[,] heightMap = terrainData.GetHeights(0,0 , terrainData.heightmapResolution, terrainData.heightmapResolution);
+
+        float[, ,] alphamapList = new float[terrainData.alphamapWidth, terrainData.alphamapHeight, terrainData.alphamapLayers];
+        
+        for(int height = 0; height < terrainData.alphamapHeight; height++)
+        {
+            for(int width = 0; width < terrainData.alphamapWidth; width++)
+            {
+                float[] alphamap = new float[terrainData.alphamapLayers];
+
+                for (int i = 0; i < terrainTextureData.Count; i++)
+                {
+                    float heightBegin = terrainTextureData[i].minHeight - terrainTextureBlendOffset;
+                    float heightEnd = terrainTextureData[i].maxHeight + terrainTextureBlendOffset;
+
+                    if (heightMap[width, height] >= heightBegin && heightMap[width, height] <= heightEnd)
+                    {
+                        alphamap[i] = 1;
+                    }
+
+                }
+
+                Blend(alphamap); 
+
+                for(int j = 0; j < terrainTextureData.Count; j++)
+                {
+                    alphamapList[width,height,j] = alphamap[j];
+                }
+
+            }
+        }
+
+        terrainData.SetAlphamaps(0,0 , alphamapList);
+
+    }
+
+
+    private void Blend(float[] alphamap)
+    {
+        float total = 0;
+
+        for(int i = 0; i < alphamap.Length; i++)
+        {
+            total += alphamap[i];
+        }
+
+        for (int i = 0; i < alphamap.Length; i++)
+        {
+            alphamap[i] = alphamap[i] / total;
+        }
+
+
+    }
+
+
 
     private void OnDestroy()
     {
